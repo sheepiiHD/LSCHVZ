77594fdb8679f0bfca7129473748c91a9a069c54
sheepiiHD <sheepiiHD@gmail.com>
2016-11-08 23:19:59 -0500

    Uploading project

---
+++ app/src/main/java/net/battleplugins/msutton/lschvz/Home.java
@@ -0,0 +1,33 @@
+package net.battleplugins.msutton.lschvz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    String name, password, email, Err;
+    TextView nameTV, emailTV, passwordTV, err;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        nameTV = (TextView) findViewById(R.id.home_name);
+        emailTV = (TextView) findViewById(R.id.home_email);
+        passwordTV = (TextView) findViewById(R.id.home_password);
+        err = (TextView) findViewById(R.id.err);
+
+        name = getIntent().getStringExtra("name");
+        password = getIntent().getStringExtra("password");
+        email = getIntent().getStringExtra("email");
+        Err = getIntent().getStringExtra("err");
+
+        nameTV.setText("Welcome " + name);
+        passwordTV.setText("Your password is " + password);
+        emailTV.setText("Your email is " + email);
+        err.setText(Err);
+
+    }
+}
